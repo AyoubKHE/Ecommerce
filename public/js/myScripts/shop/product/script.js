1f639ffcaee6cdd2bde480acb51511a5cace1837
Ayoub <itsayoubkheyar06@gmail.com>
2024-07-17 18:20:38 +0100

    commit

--- public/js/myScripts/shop/product/script.js
+++ public/js/myScripts/shop/product/script.js
@@ -1,37 +1,6 @@
 
 let productId = document.getElementById("product_id").value;
 
-let productVariations = null;
-
-// document.addEventListener("DOMContentLoaded", function () {
-
-//     axios({
-//         method: "GET",
-//         url: `http://127.0.0.1:8000/api/product/${productId}/variations`,
-//     })
-//         .then(response => {
-
-//             console.log(response);
-//             productVariations = response.data.productVariations;
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//             if (error.response.status === 401) {
-
-//                 window.location = "login";
-//             }
-//             else if (error.response.status === 403) {
-
-//                 alert("Vous n avez pas la permission de faire ça");
-//             }
-//             else {
-
-//                 alert("Y a un problème dans le systeme contacter l'admin");
-//             }
-//         })
-// })
-
-
 let enabledRadioButtons = {};
 
 let selectedOptions = {};
@@ -65,7 +34,7 @@ allAttributesRadioButtons.forEach(function (attributeRadioButton) {
 
                 document.getElementById("spinner-container").innerHTML = "";
 
-                productVariations = response.data.productVariations;
+                let productVariations = response.data.productVariations;
 
                 console.log(productVariations);
 
